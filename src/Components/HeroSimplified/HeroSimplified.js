7455ebe4143036443119e49c342401a367f40fb7
NazariiLysyi <nazariilysyiit@gmail.com>
2021-05-24 21:04:35 +0200

    added route, assets and icon

--- src/Components/HeroSimplified/HeroSimplified.js
+++ src/Components/HeroSimplified/HeroSimplified.js
@@ -1,31 +1,37 @@
 import React from 'react';
+import * as icon from '../../assets/icons';
+import { Link } from 'react-router-dom';
 import './HeroSimplified.css';
 
-
-function HeroSimplified ({ name, imgUrl, powerstats}) {
-return (
-            <div className='featured__hero'>
-                    <h2>{name}</h2>
-                    <img src={imgUrl} alt={`${name}`} />
-                    <div className='featured__hero__stats'>
-                        <div>
-                            <p>{powerstats.combat}</p>
-                        </div>
-                        <div>
-                            <p>{powerstats.durability}</p>
-                        </div>
-                        <div>
-                            <p>{powerstats.intelligence}</p>
-                        </div>
-                        <div>
-                            <p>{powerstats.speed}</p>
-                        </div>
-                        <div>
-                            <p>{powerstats.strength}</p>
-                        </div>
-                    </div>
-            </div>
-    );
+function HeroSimplified({ name, imgUrl, powerstats, id }) {
+  return (
+    <div className='featured__hero'>
+      <h2>{name}</h2>
+      <Link to={`/hero/${id}`}><img className='featured__hero__img' src={imgUrl} alt={`${name}`} /></Link>
+      <div className='featured__hero__stats'>
+        <div>
+          <img className='featured__hero__stats__icon' src={icon.combat} alt="combat icon" />
+          <p>{powerstats.combat}</p>
+        </div>
+        <div>
+          <img className='featured__hero__stats__icon' src={icon.durability} alt="combat icon" />
+          <p>{powerstats.durability}</p>
+        </div>
+        <div>
+          <img className='featured__hero__stats__icon' src={icon.intelligence} alt="combat icon" />
+          <p>{powerstats.intelligence}</p>
+        </div>
+        <div>
+          <img className='featured__hero__stats__icon' src={icon.speed} alt="combat icon" />
+          <p>{powerstats.speed}</p>
+        </div>
+        <div>
+          <img className='featured__hero__stats__icon' src={icon.strength} alt="combat icon" />
+          <p>{powerstats.strength}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroSimplified;
